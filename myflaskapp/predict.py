23527cb5cc767a756e83338c391c3be2588a2d0e
Matthew Dias <MatthewWilfredKeala.Dias@calbaptist.edu>
2019-03-16 08:59:42 -0700

    trying again

--- myflaskapp/predict.py
+++ myflaskapp/predict.py
@@ -1,22 +1,60 @@
 import pickle
 import json
+import os
 import numpy as np
+import matplotlib.pyplot as plt
 
 def predict(fileName):
     with open(fileName) as f:
         cellData = json.load(f)
-    
-    model = pickle.load(open('C:\\Users\\Matthew\\CARE Project\\CARE_Part_2\\svc_pca_mod.pkl', "rb"))
-    #prediction = model.predict(cellData)
-    return cellData
+
+    #model = pickle.load(open('.\\pythonScripts\\svc_pca_mod.pkl', "rb"))
+    model = pickle.load(open('.\\pythonScripts\\forest_pca_mod.pkl', "rb"))
+
+    cellList = getData(cellData)
+    prediction = model.predict(cellList)
+    return prediction
+
+#get passed amount of pca features for cell
+def getData(cellData):
+    data = []
+    for n in cellData:
+        data.append(cellData[n])
+    data = np.array(data).transpose()
+    return data
+
+
+def saveGraph(test_Y, pred_Y):
+    time = populateTime(len(pred_Y))
+    plt.scatter(time, test_Y, color='black')
+    plt.plot(time, pred_Y, color='blue', linewidth=3)
+    plt.title("Predicted Cell Growth Velocity")
+    plt.xticks(np.arange(0, 200, step=20))
+    plt.yticks(np.arange(-5, 20, step=2))
+    plt.xlabel("time")
+    plt.ylabel("velocity")
+
+    #my_path
+    plt.savefig('./static/graphs/newFigure.png')
+
+def populateTime(time):
+    timeArray = []
+    for i in range(time):
+        timeArray.append(i)
+    return timeArray
+
+def splitPred(pred_val):
+    with open("inputTestData.json") as f:
+        sample_data = json.load(f)
+
+    values = []
+    values.append(pred_val[:len(hello) - 1])
+    true_val = sample_data["Feature3"]
+    values.append(np.array(true_val[1:]).transpose())
+    return values
+
 
 hello = predict('inputTestData.json')
+values = splitPred(hello)
 
-def getPercentFeatureData(train, featureData, percent):
-    combinedFeatures = []
-    for feature in featureData:
-        if train:
-            combinedFeatures.append(feature[:int(len(feature) * percent)])
-        else:
-            combinedFeatures.append(feature[int(len(feature) * percent + 1):int(len(feature) - 1)])
-    return combinedFeatures
+saveGraph(values[1], values[0])
